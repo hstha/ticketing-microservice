e2ccd5971f3c0c9609bad700423d25266bfcc964
hstha <shresthaheriz15@gmail.com>
2022-02-04 19:39:57 +0545

    - fixed typo on auth-depl  - added ingress-srv  - added routes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,11 @@ import { json } from "body-parser";
 const app = express();
 app.use(json());
 
+app.get("/api/users/currentuser", (req, res) => {
+  console.log("hello");
+  res.send("Hi there!");
+});
+
 app.listen(3000, () => {
   console.log("Auth server is running");
 });
