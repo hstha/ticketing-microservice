5e743dc112f411297b5e9327e6241bf2b6ce8323
hstha <shresthaheriz15@gmail.com>
2022-02-07 15:18:35 +0545

    added not authorized error and require auth middleware

--- common/src/errors/index.ts
+++ common/src/errors/index.ts
@@ -3,3 +3,4 @@ export * from "./request-validation-error";
 export * from "./custom-error";
 export * from "./not-found";
 export * from "./bad-request";
+export * from "./not-authorized-error";
--- common/src/middleware/current-user.ts
+++ common/src/middleware/current-user.ts
@@ -10,6 +10,7 @@ declare global {
   namespace Express {
     interface Request {
       currentUser?: UserPayload;
+      session?: any;
     }
   }
 }
